06007c8a80510456358dd9e928da12f88d5876ce
luiza097 <luizamelo097@gmail.com>
2025-09-19 19:51:30 -0300

    questão 09 e 08

---
+++ Unidade01/ListaPonteiros/questao08.c
@@ -0,0 +1,9 @@
+//*(pulo + 2); <--- ALTERNATIVA CORRETA
+//*(pulo + 4);
+//pulo + 4;
+//pulo + 2;
+
+/* *(pulo + 2): O terceiro elemento do vetor
+vai estar no indice 2. Pulo + 2 faz o vetor caminhar para
+o indice 2. 
+*/
---
+++ Unidade01/ListaPonteiros/questao09.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//p = mat + 1; // VÁLIDA: p vai guardar o endereço do segundo elemento
+//p = mat++; // INVÁLIDA: não é possível modificar o valor de mat
+//p = ++mat; // INVÁLIDA: não é possível modificar o valor de mat
+//x = (*mat); // VÁLIDA: Retorna o valor do primeiro elemento do vetor mat
+
+/*
+p = mat + 1 é válido pois é uma aritimética de ponteiros, dessa forma 
+é possível "caminhar" pelos indices do vetor
+As expressões p = mat++ e p = ++mat não são válidas pq tentam alterar
+o valor de mat
+a última operação é válida pois estamos atribuindo um valor int para
+uma variável do tipo int
+*/
